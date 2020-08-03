61375f504793c75855919b59efc3f854439a635c
Imahashi Yumiko <hc.imahashi@gmail.com>
2020-08-03 17:25:44 +0900

    課題1ｰ1. 既存の実行コードのテスト

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,9 +1,18 @@
 RSpec.describe FizzBuzz do
+  require "fizz_buzz"
+
   it "has a version number" do
     expect(FizzBuzz::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "課題１−１：#responseがそのままの値を返すこと" do
+    it "'1'は'1'を返す" do
+      fizzbuzz= FizzBuzz.new('1')
+      expect(fizzbuzz.response).to eq '1'
+    end
+    it "'9999999998'は'9999999998'を返す" do
+      fizzbuzz= FizzBuzz.new('9999999998')
+      expect(fizzbuzz.response).to eq '9999999998'
+    end
   end
 end

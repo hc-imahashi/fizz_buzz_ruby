be3fa26f3106f29dc7810425d08685cb8ff5a903
Imahashi Yumiko <hc.imahashi@gmail.com>
2020-08-04 11:19:40 +0900

    課題1-2：不正入力に対してエラー【Green】

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -4,17 +4,23 @@ class InputError < StandardError; end
 
 class FizzBuzz
   def initialize str
-    @input_str = str
+    @input_number = str.split.first.to_i
   end
 
   def response
     raise InputError unless valid?
-    @input_str
+    @input_number.to_s
   end
 
   private
   def valid?
-    true
+    if @input_number.nil?
+      false
+    elsif @input_number > 0
+      true
+    else
+      false
+    end
   end
 
 end
